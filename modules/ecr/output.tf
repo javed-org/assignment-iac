5f6f43aa500358bacb63366635d9efe8c21cb41a
javed-digitify <158244935+javed-digitify@users.noreply.github.com>
2024-01-31 13:55:02 +0500

    Update output.tf

--- modules/ecr/output.tf
+++ modules/ecr/output.tf
@@ -1,6 +1,6 @@
 output "ecr_repo_names" {
   value = aws_ecr_repository.repo[*].name
 }
-output "ecr_repo_arns"{
-value= aws_ecr_repository.repo[*].arn
+output "ecr_repo_urls"{
+value= aws_ecr_repository.repo[*].repository_url
 }
