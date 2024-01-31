92cfad0cd3b2d6a216ff466a7f650d85b1f04d50
javed-digitify <158244935+javed-digitify@users.noreply.github.com>
2024-01-31 13:55:41 +0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,7 +4,7 @@ output "eks-cluster-name" {
 output "ecr_repo_names" {
   value = module.ecr.ecr_repo_names
 }
-output "ecr_repo_arns" {
-  value = module.ecr.ecr_repo_arns
+output "ecr_repo_urls" {
+  value = module.ecr.ecr_repo_urls
 }
 
